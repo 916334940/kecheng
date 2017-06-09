60167bee4c251ff490964c5c63ef1a5a26767bea
916334940 <916334940@qq.com>
2017-06-09 20:06:45 +0800

    2

--- WindowsFormsApplication1/Form4.cs
+++ WindowsFormsApplication1/Form4.cs
@@ -66,30 +66,7 @@ namespace WindowsFormsApplication1
 
         private void Form4_Load(object sender, EventArgs e)
         {
-            string constr = ConfigurationManager.AppSettings["ConnectionString"];
-            try
-            {
-                SqlConnection conn = new SqlConnection(constr);
-
-                conn.Open();
-
-                string cmdStr1 = "select count(*) from xuanze";
-                SqlDataAdapter da = new SqlDataAdapter(cmdStr1, conn);
-                DataSet ds = new DataSet();
-                 da.Fill(ds);
-                int id22 = Convert.ToInt16( ds.Tables[0].Rows[0][0]);
-
-                MessageBox.Show("当前选择题数有"+id22+"个");
-               
-
-
-                conn.Close();
-
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show("" + error);
-            }
+           
             
         }
 
